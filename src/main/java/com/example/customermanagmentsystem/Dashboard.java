042cb0d2a4c44b000b96b6d3126c773779a2af59
moham <mohamadbayram52@gmail.com>
2024-06-16 17:40:22 +0200

    initialization of the home scene

--- src/main/java/com/example/customermanagmentsystem/Dashboard.java
+++ src/main/java/com/example/customermanagmentsystem/Dashboard.java
@@ -10,11 +10,11 @@ import java.io.IOException;
 public class Dashboard extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Dashboard.class.getResource("log_in.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Dashboard.class.getResource("log_in.fxml"));//log_in.fxml
         Scene scene = new Scene(fxmlLoader.load(), 450, 350);
         stage.setTitle("Customer Managment System");
         stage.setScene(scene);
-        stage.setResizable(false);
+        //stage.setResizable(false);
         stage.show();
     }
 
